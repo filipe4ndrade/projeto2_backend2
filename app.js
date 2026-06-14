b30523ae1c33109dd745707c0d832907284015b4
José Filipe <filipeandrade000@gmail.com>
2026-06-14 16:21:28 -0300

    corrigir swagger host dinamico e adicionar session middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 var passport = require('passport');
 var swaggerUi = require('swagger-ui-express');
 var swaggerFile = require('./config/swagger_output.json');
@@ -16,6 +17,11 @@ var infoRoutes = require('./routes/info');
 var petRoutes = require('./routes/pets');
 var atendimentoRoutes = require('./routes/atendimentos');
 var usuarioRoutes = require('./routes/usuarios');
+
+var emProd = process.env.ENV === 'prod';
+
+swaggerFile.host = process.env.APP_HOST || 'localhost:3000';
+swaggerFile.schemes = [emProd ? 'https' : 'http'];
 
 var app = express();
 
@@ -29,6 +35,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: process.env.COOKIE_SECURE === 'true',
+    maxAge: parseInt(process.env.COOKIE_MAX_AGE, 10),
+  },
+}));
 
 app.use(passport.initialize());
 configurarPassport(passport);
